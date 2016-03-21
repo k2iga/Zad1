95cec9655cc2730b99e914be08a5e8871d32d652
k2iga <k2iga7@gmail.com>
2016-03-21 15:49:07 +0100

    Zawsze dodaje komentarze

---
+++ projekt/Main.cpp
@@ -0,0 +1,22 @@
+//To jest plik main ale tu nic nie bêdzie 
+//albo prawie nic nie bêdzie
+#include<iostream>
+#include "Menu.h"
+#include "Polynomial_equation.h"
+
+
+
+
+int main()
+
+{
+
+
+	Menu pokaz;
+	pokaz.showMenu();
+	
+
+ 
+	system("pause");
+}
+
